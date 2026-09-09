cca8c0fcce98933d1d301a169a1195c0a427f536
NCombarieu <55128932+NCombarieu@users.noreply.github.com>
2026-09-09 22:49:40 +0200

    container: run daemon config templates directly

--- containers/entrypoint.sh
+++ containers/entrypoint.sh
@@ -3,17 +3,20 @@ set -eu
 
 role="${1:-arbiter}"
 shift || true
+config_root="${SHINKEN_CONFIG:-/etc/shinken}"
 
 case "$role" in
   arbiter)
     if [ "$#" -eq 0 ]; then
-      set -- -c "${SHINKEN_CONFIG:-/etc/shinken}/shinken.cfg"
+      echo "arbiter requires one or more -c configuration files" >&2
+      echo "example: arbiter -c ${config_root}/shinken-specific.cfg" >&2
+      exit 64
     fi
     exec shinken-arbiter "$@"
     ;;
   broker|poller|reactionner|receiver|scheduler)
     if [ "$#" -eq 0 ]; then
-      set -- -c "${SHINKEN_CONFIG:-/etc/shinken}/daemons/${role}d.ini"
+      set -- -c "${config_root}/daemons/${role}d.ini.in"
     fi
     exec "shinken-${role}" "$@"
     ;;
