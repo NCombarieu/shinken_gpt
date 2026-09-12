45cbe6587a5d7f4244df37b33d112f0f73b67408
NCombarieu <55128932+NCombarieu@users.noreply.github.com>
2026-09-12 15:20:48 +0200

    Modernize service dependency mapping helper for Python 3

--- libexec/service_dependency_mapping.py
+++ libexec/service_dependency_mapping.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # This file is part of Shinken.
 #
@@ -16,255 +16,258 @@
 # along with Shinken.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-This program get hosts informations from running arbiter daemon and 
-get service dependencies definition from config pack flat files then
-dump services dependencies according to the config files to a json
-that can be loaded in hot_dependencies_arbiter module.
-
-servicedependencies file in pack use template host_name that will be
-matched in hosts 'use' directive to apply those servicedependency
-definition to hosts.
-
+Build a service-dependency mapping from a running arbiter and pack config.
 """
 
+import json
+import optparse
+import shutil
+import sys
 
 from shinken.objects.arbiterlink import ArbiterLink
-import os, sys, optparse, cPickle, shutil
-import shinken.daemons.arbiterdaemon
-from shinken.arbiterlink import ArbiterLink
-from shinken.http_client import HTTPExceptions 
-from shinken.log import logger
 from shinken.objects.config import Config
 
-# Try to load json (2.5 and higer) or simplejson if failed (python2.4)
-try:
-    import json
-except ImportError:
-    # For old Python version, load simple json
-    try:
-        import simplejson as json
-    except ImportError:
-        raise SystemExit("Error: you need the json or simplejson module "
-                         "for this script")
-
-
-sat_types = ['arbiter', 'scheduler', 'poller', 'reactionner',
-             'receiver', 'broker']
 
 VERSION = '0.2'
 
-class ShinkenAdmin():
+
+class ShinkenAdmin:
 
     def __init__(self):
-        self.arb = None 
+        self.arb = None
         self.conf = None
         self.addr = 'localhost'
         self.port = '7770'
         self.arb_name = 'arbiter-master'
 
     def do_connect(self, verbose=False):
-        '''
-        Connect to an arbiter daemon
-        Syntax: connect [host]:[port]
-        Ex: for Connecting to server, port 7770
-        > connect server:7770
-        Ex: connect to localhost, port 7770
-        > connect
-        '''
-    
+        """Connect to an arbiter daemon."""
         if verbose:
-            print "Connection to %s:%s" % (self.addr, self.port)
+            print("Connection to %s:%s" % (self.addr, self.port))
         ArbiterLink.use_ssl = False
-        self.arb = ArbiterLink({'arbiter_name': self.arb_name, 'address': self.addr, 'port': self.port})
+        self.arb = ArbiterLink({
+            'arbiter_name': self.arb_name,
+            'address': self.addr,
+            'port': self.port,
+        })
         self.arb.fill_default()
         self.arb.pythonize()
         self.arb.update_infos()
         if not self.arb.reachable:
             sys.exit("Connection to the arbiter got a problem")
-        print "Connection OK"
-    
+        print("Connection OK")
+
     def getconf(self, config):
-        '''
-        Get the data in the arbiter for a table and some properties
-        like hosts  host_name realm
-        '''
-        files = [config]
+        """Get hosts and service dependencies from the arbiter/config."""
         conf = Config()
         conf.read_config_silent = 1
 
-        # Get hosts objects
-        properties = [ 'host_name','use','act_depend_of']
+        properties = ['host_name', 'use', 'act_depend_of']
         hosts = self.arb.get_objects_properties('hosts', properties)
 
-        # Get services dependencies
-        svcdep_buf = conf.read_config(files)
+        svcdep_buf = conf.read_config([config])
         svc_dep = conf.read_config_buf(svcdep_buf)['servicedependency']
-
-        return (hosts, svc_dep)
+        return hosts, svc_dep
 
     def load_svc_mapping(self, hosts, svc_dep, verbose=False):
-        '''
-        Make tuples mapping service dependencies. Return a list of tuples 
-        and need hosts and service dependencies parameter.
-        '''
-        r = []
-        # Search for host matching "use" template
+        """Build tuples representing service dependencies."""
+        result = []
         for dep in svc_dep:
-            # Get host_name and dependent_host_name field from servicedependency
-            # config file in packs. Usually values are host's pack template.
             parent_host_name = self.split_and_merge(dep['host_name'])
             try:
-                dependent_host_name = self.split_and_merge(dep['dependent_host_name'])
+                dependent_host_name = self.split_and_merge(
+                    dep['dependent_host_name']
+                )
             except KeyError:
                 dependent_host_name = parent_host_name
-            if verbose:
-                print ""
-                print 'Service dependency host_name', parent_host_name
-                print 'Service dependency dependent_host_name', dependent_host_name
 
-            # Make list before process them by splitting comma separated values.
-            dep['service_description'] = self.split_and_merge(dep['service_description'])
-            dep['dependent_service_description'] = self.split_and_merge(dep['dependent_service_description'])
-            # Construct dependencies tuples
-            # Search in host all hosts that use template host_name
+            if verbose:
+                print()
+                print('Service dependency host_name', parent_host_name)
+                print(
+                    'Service dependency dependent_host_name',
+                    dependent_host_name,
+                )
+
+            dep['service_description'] = self.split_and_merge(
+                dep['service_description']
+            )
+            dep['dependent_service_description'] = self.split_and_merge(
+                dep['dependent_service_description']
+            )
+
             parent_svc_tuples = []
             dependent_svc_tuples = []
             for parent_svc in dep['service_description']:
-                parent_svc_tuples += [[ ('service', host[0] + "," + parent_svc) for host in hosts if host_name in host[1] ] for host_name in parent_host_name ]
+                parent_svc_tuples += [
+                    [
+                        ('service', host[0] + "," + parent_svc)
+                        for host in hosts
+                        if host_name in host[1]
+                    ]
+                    for host_name in parent_host_name
+                ]
             for dependent_svc in dep['dependent_service_description']:
-                dependent_svc_tuples += [[ ('service', host[0] + "," + dependent_svc) for host in hosts if host_name in host[1] ] for host_name in dependent_host_name ]
+                dependent_svc_tuples += [
+                    [
+                        ('service', host[0] + "," + dependent_svc)
+                        for host in hosts
+                        if host_name in host[1]
+                    ]
+                    for host_name in dependent_host_name
+                ]
 
-            # No need to separate tuples by services here so we merge them
-            dependent_tuples = self.split_and_merge(dependent_svc_tuples, split=False)
+            dependent_tuples = self.split_and_merge(
+                dependent_svc_tuples, split=False
+            )
 
             if verbose:
-                print 'Parent service dependencies tuples list', parent_svc_tuples
-                print 'Dependent service dependencies tuples list', dependent_svc_tuples
+                print(
+                    'Parent service dependencies tuples list',
+                    parent_svc_tuples,
+                )
+                print(
+                    'Dependent service dependencies tuples list',
+                    dependent_svc_tuples,
+                )
 
-            # Process !
             for parent_tuples in parent_svc_tuples:
-                r.append(self.make_all_dep_tuples(hosts, parent_tuples, dependent_tuples))
+                result.append(
+                    self.make_all_dep_tuples(
+                        hosts, parent_tuples, dependent_tuples
+                    )
+                )
 
         if verbose:
-            print ""
-            print "Result:", r
-        return r
+            print()
+            print("Result:", result)
+        return result
 
-    def make_all_dep_tuples(self, hosts, parent_tuples=[()], dependent_tuples=[[()]] ):
-        '''
-        List imbrication : List_by_services : [ List_by_hosts : [ Service_dependency_tuples : ( ) ] ]
-        '''
-        res = []
-        for ptuple in parent_tuples:
-            parent = { 'host_name' : self.get_dependency_tuple_host_name(ptuple), 'svc_desc' : self.get_dependency_tuple_service_description(ptuple) }
-            # Dive into dependent services
-            for dtuple in dependent_tuples:
-                dependent = { 'host_name' : self.get_dependency_tuple_host_name(dtuple), 'svc_desc' : self.get_dependency_tuple_service_description(dtuple) }
-                dependent['host_object'] = next( host for host in hosts if host[0] == dependent['host_name'] )
-                res = self.make_dep_tuple(parent, dependent, ptuple, dtuple, res)
+    def make_all_dep_tuples(
+        self, hosts, parent_tuples=None, dependent_tuples=None
+    ):
+        """Build all dependency tuples for the supplied parent/dependents."""
+        if parent_tuples is None:
+            parent_tuples = [()]
+        if dependent_tuples is None:
+            dependent_tuples = [[()]]
 
-        return res
+        result = []
+        for parent_tuple in parent_tuples:
+            parent = {
+                'host_name': self.get_dependency_tuple_host_name(parent_tuple),
+                'svc_desc': self.get_dependency_tuple_service_description(
+                    parent_tuple
+                ),
+            }
+            for dependent_tuple in dependent_tuples:
+                dependent = {
+                    'host_name': self.get_dependency_tuple_host_name(
+                        dependent_tuple
+                    ),
+                    'svc_desc': self.get_dependency_tuple_service_description(
+                        dependent_tuple
+                    ),
+                }
+                dependent['host_object'] = next(
+                    host for host in hosts
+                    if host[0] == dependent['host_name']
+                )
+                result = self.make_dep_tuple(
+                    parent,
+                    dependent,
+                    parent_tuple,
+                    dependent_tuple,
+                    result,
+                )
+        return result
 
-    def make_dep_tuple(self, parent, dependent, ptuple, dtuple, res):
-        '''
-        Search host dependency and make tuple according to it.
-        '''
+    def make_dep_tuple(
+        self, parent, dependent, parent_tuple, dependent_tuple, result
+    ):
+        """Match a host dependency and return its service tuple."""
         try:
-            dependent_host_parent = self.get_host_dependency(dependent['host_object'])
+            dependent_host_parent = self.get_host_dependency(
+                dependent['host_object']
+            )
             if parent['host_name'] == dependent_host_parent:
-                res = (ptuple, dtuple)
+                result = (parent_tuple, dependent_tuple)
         except IndexError:
             if parent['host_name'] == dependent['host_name']:
-                res = (ptuple, dtuple)
-
-        return res
+                result = (parent_tuple, dependent_tuple)
+        return result
 
     def get_host_dependency(self, dependent_host):
-        '''
-        Get parent host_name attribute of host.
-        '''
+        """Return the parent host name from a host dependency."""
         return dependent_host[2][0][0].host_name
 
-    def get_dependency_tuple_host_name(self, tuple):
-        '''
-        Just get the host name part of a dependency tuple.
-        A dependency tuples is : ( 'service', 'host_name, service_description' )
-        '''
-        return tuple[1].split(',')[0]
+    def get_dependency_tuple_host_name(self, dependency_tuple):
+        """Return the host-name part of a dependency tuple."""
+        return dependency_tuple[1].split(',')[0]
 
-    def get_dependency_tuple_service_description(self, tuple):
-        '''
-        Just get the service description part of a dependency tuple.
-        A dependency tuples is : ( 'service', 'host_name, service_description' )
-        '''
-        return tuple[1].split(',')[1]
+    def get_dependency_tuple_service_description(self, dependency_tuple):
+        """Return the service-description part of a dependency tuple."""
+        return dependency_tuple[1].split(',')[1]
 
-
-    def split_and_merge(self, list, split=True):
-        '''
-        Split a list on comma separator and merge resulting lists
-        into an uniq list then return it
-        '''
-        res = []
-        for elt in list:
+    def split_and_merge(self, values, split=True):
+        """Split comma-separated values and flatten the result."""
+        result = []
+        for value in values:
             if split:
-                res += elt.split(',')
+                result += value.split(',')
             else:
-                res += elt
-        return res
+                result += value
+        return result
 
-    def clean_empty_value(self, r):
-        '''
-        Empty value comes from unused config pack and then service dep
-        is created but without nothing...
-        '''
-        r_cleaned = []
-        for elt in r:
-            if elt != []:
-                r_cleaned.append(elt)
-
-        return r_cleaned
+    def clean_empty_value(self, result):
+        """Drop empty mappings produced by unused config packs."""
+        return [value for value in result if value != []]
 
     def main(self, output_file, config, verbose):
         self.do_connect(verbose)
 
-        # Get needed conf
         hosts, svc_dep = self.getconf(config)
         if verbose:
-            print "Hosts:", hosts
-            print "Service Dep:", svc_dep
+            print("Hosts:", hosts)
+            print("Service Dep:", svc_dep)
 
-        # Make the map
-        r = self.load_svc_mapping(hosts, svc_dep, verbose)
+        result = self.load_svc_mapping(hosts, svc_dep, verbose)
+        result = self.clean_empty_value(result)
 
-        # Clean mapping from empty value
-        r = self.clean_empty_value(r)
-
-        # Write ouput file
+        temp_file = output_file + '.tmp'
         try:
-            f = open(output_file + '.tmp', 'wb')
-            buf = json.dumps(r)
-            f.write(buf)
-            f.close()
-            shutil.move(output_file + '.tmp', output_file)
-            print "File %s wrote" % output_file
-        except IOError, exp:
+            with open(temp_file, 'w', encoding='utf-8') as stream:
+                json.dump(result, stream)
+            shutil.move(temp_file, output_file)
+            print("File %s written" % output_file)
+        except OSError as exp:
             sys.exit("Error writing the file %s: %s" % (output_file, exp))
-        jsonmappingfile = open(output_file, 'w')
-        try:
-            json.dump(r, jsonmappingfile)
-        finally:
-            jsonmappingfile.close()
 
 
 if __name__ == "__main__":
     parser = optparse.OptionParser(
-        version="Shinken service hot dependency according to packs (or custom) definition to json mapping %s" % VERSION)
-    parser.add_option("-o", "--output", dest='output_file',
-                      default='/tmp/shinken_service_dependency:mapping.json',
-                      help="Path of the generated json mapping file.")
-    parser.add_option('-c', '--config', dest='config', help='Shinken main config file.')
-    parser.add_option('-v', '--verbose', action='store_true', dest='verbose', help='More verbosity. Used to debug')
+        version=(
+            "Shinken service hot dependency according to packs (or custom) "
+            "definition to json mapping %s" % VERSION
+        )
+    )
+    parser.add_option(
+        "-o",
+        "--output",
+        dest='output_file',
+        default='/tmp/shinken_service_dependency:mapping.json',
+        help="Path of the generated json mapping file.",
+    )
+    parser.add_option(
+        '-c', '--config', dest='config', help='Shinken main config file.'
+    )
+    parser.add_option(
+        '-v',
+        '--verbose',
+        action='store_true',
+        dest='verbose',
+        help='More verbosity. Used to debug',
+    )
 
     opts, args = parser.parse_args()
     if args:
