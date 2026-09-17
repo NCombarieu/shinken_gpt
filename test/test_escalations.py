435213f7759cce0898549819765cf9bb4defb39f
NCombarieu <55128932+NCombarieu@users.noreply.github.com>
2026-09-17 22:50:02 +0200

    test: remove six compatibility import from test/test_escalations.py

--- test/test_escalations.py
+++ test/test_escalations.py
@@ -26,7 +26,6 @@ from __future__ import print_function
 from __future__ import absolute_import
 from shinken_test import *
 from shinken.objects.serviceescalation import Serviceescalation
-from six.moves import range
 
 class TestEscalations(ShinkenTest):
 
