3aef1288c71a421160fab5196f86bd251eb4f916
NCombarieu <55128932+NCombarieu@users.noreply.github.com>
2026-09-17 22:49:57 +0200

    test: remove six compatibility import from test/test_clean_sched_queues.py

--- test/test_clean_sched_queues.py
+++ test/test_clean_sched_queues.py
@@ -25,7 +25,6 @@
 from __future__ import print_function
 from __future__ import absolute_import
 from shinken_test import *
-from six.moves import range
 
 
 class TestSchedCleanQueues(ShinkenTest):
